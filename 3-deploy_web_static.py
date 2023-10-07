1d73f6ee55d47be4f92f5d2720d097d392b1dfe1
Sharon <Sharonwanjiru392@gmail.com>
2023-10-07 12:51:55 -0700

    0-setup_web_static.sh

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -2,8 +2,8 @@
 """ Fabric script (based on the file 2-do_deploy_web_static.py) that creates
 and distributes an archive to your web servers, using the function deploy: """
 
-
-from fabric.api import *
+from fabric.api import local
+from fabric.operations import env, put, run
 from datetime import datetime
 from os.path import exists
 # do_pack = __import__('1-pack_web_static').do_pack
